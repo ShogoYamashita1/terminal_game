ec63bde8e31be2770364f0b5f5e60d50a60284f9
Shogo Yamashita <s24c1130pf@s.chibakoudai.jp>
2026-01-14 01:36:57 +0900

    画面構成のアタリ付け.エネミーの仮追加

--- micro_game.c
+++ micro_game.c
@@ -1,9 +1,18 @@
 #include <ncurses.h>
 
+
 typedef struct {
     int x, y;
+    int h, w;
     int running;
 } Game;
+
+typedef struct {
+    int x, y;
+    int v;
+    int pop;
+} Enemy;
+
 
 void init(void) {
     initscr();
@@ -28,25 +37,44 @@ void input(Game *g) {
     }
 }
 
-void update(Game *g) {
-    // 最小構成なので空
+void update(Game *g, Enemy *e) {
+    if (g->y < 2) g->y = 2;
+    if (g->y >= g->h - 2) g->y = g->h - 3;
+
+    if (g->x < 0) g->x = 0;
+    if (g->x >= g->w) g->x = g->w - 1;
+
+    if(e->pop){
+        e->v++;
+        e->y = e->v;
+    }
+    if(e->y == g->h - 3)e->pop = 0;
 }
 
-void draw(Game *g) {
+void draw(Game *g, Enemy *e) {
     clear();
     mvaddch(g->y, g->x, '@');
-    mvprintw(0, 0, "Move with arrows, q to quit");
+    if(e->pop)mvprintw(e->y, e->x, "<0w0>");
+    mvprintw(0, 0, "HP");
+    mvprintw(1, 0, "#######################################");
+    mvprintw(g->h - 2, 0, "################################");
+    mvprintw(g->h - 1, 0, "Move with arrows, q to quit");
     refresh();
 }
 
 int main(void) {
-    Game g = {10, 5, 1};
+    Game g = {0, 0, 0, 0, 1};
+    Enemy e = {0, 0, 1, 1};
 
     init();
+    getmaxyx(stdscr, g.h, g.w);
+    g.x = g.w / 2, g.y = g.h - 3;
+    e.x = g.w / 2;
+
     while (g.running) {
         input(&g);
-        update(&g);
-        draw(&g);
+        update(&g, &e);
+        draw(&g, &e);
     }
     cleanup();
     return 0;
