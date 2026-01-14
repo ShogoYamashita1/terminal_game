f90c0829002960cacc187c484280e82d54410083
Shogo Yamashita <s24c1130pf@s.chibakoudai.jp>
2026-01-14 23:35:56 +0900

    スタート画面追加.アイテム効果付与.

--- micro_game.c
+++ micro_game.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <ncurses.h>
 #include <stdlib.h>
 #include <time.h>
@@ -18,7 +19,7 @@ double normalrandom(double mu, double sigma){
 
 typedef struct {
     int h, w;
-    int num;
+    int num, abs_num;
     int counter;
     int score;
     int running;
@@ -56,6 +57,23 @@ void cleanup(void) {
     endwin();
 }
 
+void stert(Game *g) {
+    clear();
+    do{
+    int ch = getch();
+    mvprintw(g->h / 2 - 4, g->w / 2, "Move with arrows, q to quit");
+
+    mvprintw(g->h / 2- 2,  g->w / 2,  "[ITEMS]");
+    mvprintw(g->h / 2- 1,  g->w / 2,  "(.) :SCORE +10");
+    mvprintw(g->h / 2,     g->w / 2 , "(*) :HP Heal");
+    mvprintw(g->h / 2 + 1, g->w / 2,  "(@) :HP Up");
+
+    mvprintw(g->h / 2 + 3, g->w / 2, "Press to SPACE start");
+    refresh();
+    if(ch == ' ')break;
+    }while(1);
+}
+
 void input(Game *g, Player *p) {
     int ch = getch();
     switch (ch) {
@@ -73,6 +91,19 @@ void update(Game *g, Player *p, Enemy *e, Item *i) {
     if(e->x <= p->x && p->x <= e->x + e->size  &&  e->y == p->y){
         p->hp--;
     }
+
+    if(i->x <= p->x && p->x <= i->x + 2  &&  i->y == p->y){
+        if(g->abs_num > 5 && 8 > g->abs_num){
+            g->score += 10;
+        }if(g->abs_num > 8 && 10 > g->abs_num){
+            if(p->hp < 3){
+                p->hp++;
+            }
+        }if(g->abs_num > 10){
+            p->hp++;
+        }
+    }
+
     g->counter++;
 
 //--PLAYER
@@ -84,7 +115,7 @@ void update(Game *g, Player *p, Enemy *e, Item *i) {
 //--ENEMY
     if(e->pop){
         if(g->counter == 1){
-            e->y++;
+            e->y += 2;
         }
     }else{// repop
         e->pop = 1;
@@ -99,13 +130,14 @@ void update(Game *g, Player *p, Enemy *e, Item *i) {
     if(i->pop){
         if(g->counter == 2){
             i->y++;
-            g->num = normalrandom(0, 8);//
             g->counter = 0;
         }
     }else{// repop
         i->pop = 1;
         i->y = 0;
-        //g->num = normalrandom(0, 3);
+        g->num = normalrandom(0, 8);//
+        g->abs_num = fabs(g->num);//
+        i->x = getrandom(0, g->w - 1);
     }
     if(i->y == g->h - 3)i->pop = 0;
 }
@@ -125,20 +157,22 @@ void draw(Game *g, Player *p, Enemy *e, Item *i) {
     }
 
     if(i->pop){
-        double abs_num = fabs(g->num);
-        if(abs_num > 5){
+        if(g->abs_num > 5)
+            mvprintw(i->y, i->x, "(.)");
+        if(g->abs_num > 8){
             mvprintw(i->y, i->x, "(*)");
-        }if(abs_num > 8){
+        }if(g->abs_num > 10){
             mvprintw(i->y, i->x, "(@)");
         }else{
-            mvprintw(i->y, i->x, "(.)");
         }
     }
 
     mvprintw(0, 0, "score:%d", g->score);
     mvprintw(1, 0, "HP:%d", p->hp);
-    mvprintw(2, 0, "num:%f", fabs(g->num));
-    mvhline(3, 0, '#', g->w);
+    mvprintw(2, 0, ":Uniform random numbers(Enemy x axis):%d", e->x);
+    mvprintw(3, 0, ":Uniform random numbers(Enemy size):%d", e->size);
+    mvprintw(4, 0, "Normal random numbers(Item type):%f", fabs(g->num));
+    mvhline(5, 0, '#', g->w);
 
     mvhline(g->h - 2, 0, '#', g->w);
     mvprintw(g->h - 1, 0, "Move with arrows, q to quit");
@@ -147,22 +181,25 @@ void draw(Game *g, Player *p, Enemy *e, Item *i) {
 
 
 int main(void) {
-    Game g = {0, 0, 0, 0, 0, 1};
+    Game g = {0, 0, 0, 0, 0, 0, 1};
     Player p = {0, 0, 3};
     Enemy e = {0, 0, 5, 1};
     Item i = {0, 0, 0, 1};
 
     init();
-    timeout(30);   // 最大30msgetch()が待つ
+    timeout(30);
 
     getmaxyx(stdscr, g.h, g.w);
     p.x = g.w / 2, p.y = g.h - 3;
     e.x = g.w / 2, i.x = g.w / 2 - 3;
 
+    stert(&g);
+
     while (g.running) {
         input(&g, &p);
         update(&g, &p, &e, &i);
         draw(&g, &p, &e, &i);
+        if(p.hp <= 0)break;
     }
     cleanup();
     printf("SCORE: %d\n", g.score);
